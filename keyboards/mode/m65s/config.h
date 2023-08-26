5bc847896a8ea5c03b6ac9e2d65c4b6f13374c8e
Manfried Ditsch <manfried.ditsch@posteo.de>
2023-08-27 00:54:37 +0200

    ModeS

--- keyboards/mode/m65s/config.h
+++ keyboards/mode/m65s/config.h
@@ -47,3 +47,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 
 #define TAP_CODE_DELAY 50
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
