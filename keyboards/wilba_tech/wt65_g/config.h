3f54a0fd43e079944516784f7d1ada5156d9e0e8
Manfried Ditsch <manfried.ditsch@posteo.de>
2023-08-27 00:57:34 +0200

    AIS65

--- keyboards/wilba_tech/wt65_g/config.h
+++ keyboards/wilba_tech/wt65_g/config.h
@@ -20,3 +20,8 @@
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+#define TAP_CODE_DELAY 50
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
