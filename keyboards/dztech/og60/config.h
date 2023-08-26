c65bcdf14d904ce39c09236fb7fc1ae926eaed8c
Manfried Ditsch <manfried.ditsch@posteo.de>
2023-08-27 00:00:22 +0200

    tofu60 2.0

---
+++ keyboards/dztech/og60/config.h
@@ -0,0 +1,5 @@
+
+#define TAP_CODE_DELAY 50
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
